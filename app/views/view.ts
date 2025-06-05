1f477c404a4b00f740d9643210d6c2f47ae135ff
LeonardoLobas <leonardolobas2703@gmail.com>
2025-06-05 17:59:01 -0300

    refactor: altera a classe View para ser abstrata e define o método template como abstrato

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
     protected elemento: HTMLElement;
 
     constructor(seletor: string) {
@@ -9,8 +9,5 @@ export class View<T> {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
-    template(model: T): string {
-        throw Error("Classe filha precisa do método template");
-    }
+    abstract template(model: T): string;
 }
-(" commit diario");
