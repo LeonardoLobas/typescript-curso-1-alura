c413b11a34712542fccdb14e5265b593a653e5f1
LeonardoLobas <leonardolobas2703@gmail.com>
2025-06-11 16:42:56 -0300

    refactor: melhora a verificação de elementos no DOM e adiciona tratamento de erro no construtor da classe View

--- app/views/view.ts
+++ app/views/view.ts
@@ -3,7 +3,12 @@ export abstract class View<T> {
     private escapar: boolean = false;
 
     constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error("Seletor não existe no dom");
+        }
         if (escapar) {
             this.escapar = escapar;
         }
