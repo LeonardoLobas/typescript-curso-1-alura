b6e13abe93f3d84d11c1cd9ec0256cd9ea52be81
LeonardoLobas <leonardolobas2703@gmail.com>
2025-06-24 10:01:00 -0300

    refactor: ajusta a assinatura do decorator inspect para remover chamada desnecessária

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -16,8 +16,8 @@ export abstract class View<T> {
             this.escapar = escapar;
         }
     }
-    @inspect()
     @logarTempoDeExecucao(true)
+    @inspect
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
